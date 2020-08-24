6170eea24aea0fbb69f175250bfd55417537ffac
kevin <kevinboucly9@gmail.com>
2020-08-24 15:11:57 +0200

    utilisation de Route pour conditionner du JSX

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Nav from 'src/components/Nav';
 import Page from 'src/components/Page';
 import Recipe from 'src/components/Recipe';
-// import Home from 'src/components/Home';
+import Home from 'src/components/Home';
 
 import recipes from 'src/data';
 
 const App = () => (
   <div>
     <Nav recipes={recipes} />
-    {/* <Page>
-      <Home />
-    </Page> */}
-    <Page>
-      <Recipe recipe={recipes[1]} />
-    </Page>
+    <Route path="/" exact>
+      <Page>
+        <Home />
+      </Page>
+    </Route>
+    <Route path="/recipe" exact>
+      <Page>
+        <Recipe recipe={recipes[1]} />
+      </Page>
+    </Route>
 
   </div>
 );
